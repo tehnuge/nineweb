0ac5002a7bd86e71393976bfaf30b784e85e48d2
John Duong <jhnduong@gmail.com>
2020-07-10 19:57:20 -0700

    make style changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 import * as Text from './text';
 import HeaderComp from './HeaderComp';
 import Home from './Home';
+import Button from 'react-bootstrap/Button';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,13 +18,12 @@ AOS.init();
 //const imgBg = "https://news.sophos.com/wp-content/uploads/2019/07/shutterstock_571378933-compressor.jpg"
 const App = () => {
   const titles = [
-    { name: Text.CLOUD_WEB_HOSTING, body: Text.CLOUD_WEB_HOSTING_D, bodyTitle: Text.CLOUD_WEB_HOSTING_T },
-    { name: Text.CLOUD_SERVER, body: Text.CLOUD_SERVER_D, bodyTitle: Text.CLOUD_SERVER_T },
-    { name: Text.CLOUD_MAIL, body: Text.CLOUD_MAIL_D, bodyTitle: Text.CLOUD_MAIL_T },
+    { name: "Cloud Services", body: `${Text.CLOUD_WEB_HOSTING} ${Text.CLOUD_SERVER} ${Text.CLOUD_MAIL}` },  
     { name: Text.DOMAIN, body: Text.DOMAIN_D, bodyTitle: Text.DOMAIN_T },
     { name: Text.COLO, body: Text.COLO_D, bodyTitle: Text.COLO_T },
     { name: Text.SSL, body: Text.SSL_D, bodyTitle: Text.SSL_T },
     { name: Text.DED, body: Text.DED_D, bodyTitle: Text.DED_T },
+    { name: "Contact Us", bodyTitle: "phone: +66 0 2722 9080 mobile: +66 8 5807 2443" }
   ]
 
   const cards = [
@@ -34,38 +35,32 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <div className="d-flex justify-content-between">
-            <div className="App-logo">
-              <img src="nineweb_mod.png" className="App-logo" alt="logo" />
-              <h1>{Text.NINE_WEB}</h1>
-            </div>
-            <div>
-              <span className="dropdown text-center">
-                <span className="">contact us </span>
-                <span><i class="fas fa-chevron-right"></i></span>
-                <div className="dropdown-content">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="card-text">
-                        <b>phone:</b> +66 0 2722 9080
-                      <br></br>
-                        <b>mobile:</b> +66 8 5807 2443
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </span>
+        <div className="container">
+        <header className="navbar d-flex align-items-baseline">
+          <div>
+            <img src="nineweb_mod.png" className="App-logo" alt="logo" />
+          </div>
+          <h1>{Text.NINE_WEB}</h1>
+          {/* mobile */}
+          <div className="tabs-m dropdown">
+            <i className="fas fa-bars"></i>
+            <div className="dropdown-content">
+              <ul className="list-group">
+                {titles.map(title =>
+                  <li key = {title.name} className="list-group-item">{title.name}</li>
+                )}
+              </ul>
             </div>
           </div>
-          <ul className="tabs navbar-nav-scroll">
-            <div className="titles d-flex justify-content-around">
-              {titles.map((title) => {
-                return <HeaderComp name={title.name} body={title.body} bodyTitle={title.bodyTitle} />
+          <ul className="tabs navbar-nav-scroll flex-grow-1">
+            <div className="titles d-flex justify-content-between">
+              {titles.map(title => {
+                return <HeaderComp key={title.name} name={title.name} body={title.body} bodyTitle={title.bodyTitle} />
               })}
             </div>
           </ul>
         </header>
+        </div>
         <Switch>
           <Route exact path='/'>
             <Home cards={cards} imgBg={imgBg} />
@@ -74,12 +69,8 @@ const App = () => {
             <h1>ABOUT</h1>
           </Route>
         </Switch>
-        <footer>
-          <div id="footer" className='d-flex justify-content-center'>
-            <h3>Contact Us</h3>
-          </div>
-        </footer>
       </div>
+      
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,20 @@ import Card from './Card';
 const Home = props => {
   return (
     <div className="body">
-    <div className="d-flex align-items-center justify-content-start">
-      <img data-aos="fade-left" src={props.imgBg} alt="server" className="img-fluid main-image" />
-      <h2>Leader in Cloud Web Hosting</h2>
+      <div className="container">
+        <div className="d-flex align-items-center justify-content-center">
+          <img data-aos="fade-left" src={props.imgBg} alt="server" className="main-image" />
+          <h2>Leader in Cloud Web Hosting</h2>
+        </div>
+        <div className="d-flex cards justify-content-between">
+          {props.cards.map(card => {
+            return (
+              <Card name={card.name} li={card.li} />
+            )
+          })}
+        </div>
+      </div>
     </div>
-    <div className="d-flex justify-content-between">
-      {props.cards.map(card => {
-        return (
-          <Card name={card.name} li={card.li} />
-        )
-      })}
-    </div>
-  </div>
   )
 }
 
